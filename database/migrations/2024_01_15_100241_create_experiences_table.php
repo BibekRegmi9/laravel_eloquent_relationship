12094ac649d0bb3f45f39270ad459cc9f8e0d643
Bibek <vivekregmi83@gmail.com>
2024-01-15 15:52:07 +0545

    create migration modified

--- database/migrations/2024_01_15_100241_create_experiences_table.php
+++ database/migrations/2024_01_15_100241_create_experiences_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('experiences', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->unsignedInteger('points');
         });
     }
 
