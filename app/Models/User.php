7cfe17a31c61502d0419538f0c6d888b16ca1bff
Bibek <vivekregmi83@gmail.com>
2024-01-15 17:16:01 +0545

    updation

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Profile;
+use App\Models\Experience;
+
 
 class User extends Authenticatable
 {
