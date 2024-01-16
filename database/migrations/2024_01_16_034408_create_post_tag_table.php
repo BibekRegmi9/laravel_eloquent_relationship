95c9c6086e24bf924bc7c08cb65165a9ee8f7bed
Bibek <vivekregmi83@gmail.com>
2024-01-16 09:30:31 +0545

    post tag migration table is generated

--- database/migrations/2024_01_16_034408_create_post_tag_table.php
+++ database/migrations/2024_01_16_034408_create_post_tag_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tags', function (Blueprint $table) {
+        Schema::create('post_tag', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tags');
+        Schema::dropIfExists('post_tag');
     }
 };
